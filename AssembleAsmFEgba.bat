bb570e9da2d935823fac4ed2eef818203c9f5ce0
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-03-31 11:56:05 -0400

    Commit

--- AssembleAsmFEgba.bat
+++ AssembleAsmFEgba.bat
@@ -6,14 +6,14 @@ SET LYN="C:\devkitPro\lyn.exe"
 
 
 @rem Assemble into an elf
-%as% -g -mcpu=arm7tdmi -mthumb-interwork %1 -o "%~dp0Patches/%~n1.elf"
-if exist "FE6_defs.s" (
+%as% -g -mcpu=arm7tdmi -mthumb-interwork %1 -o "%~dp0Patches\%~n1.elf"
+if exist "%~dp0Patches\FE6_defs.s" (
 	
 	@rem Assemble definitions into a .elf if exists	
-	%as% -g -mcpu=arm7tdmi -mthumb-interwork "FE6_defs.s" -o "%~dp0Patches/FE6_defs.elf"
+	%as% -g -mcpu=arm7tdmi -mthumb-interwork "%~dp0Patches\FE6_defs.s" -o "%~dp0Patches\FE6_defs.elf"
 
 	@rem Assebmle into a .lyn.event with definitions
-	%LYN% "%~dp0Patches/%~n1.elf" "%~dp0Patches/FE6_defs.elf" > "%~dp0Patches/FE6_%~n1.lyn.event"
+	%LYN% "%~dp0Patches\%~n1.elf" "%~dp0Patches\FE6_defs.elf" > "%~dp0Patches\FE6_%~n1.lyn.event"
 
 	@cd %~dp0/Patches
 	echo y | del "FE6_defs.elf"
@@ -22,13 +22,13 @@ if exist "FE6_defs.s" (
 
 @cd %~dp0
 
-if exist "FE7_defs.s" (
+if exist "%~dp0Patches\FE7_defs.s" (
 	
 	@rem Assemble definitions into a .elf if exists	
-	%as% -g -mcpu=arm7tdmi -mthumb-interwork "FE7_defs.s" -o "%~dp0Patches/FE7_defs.elf"
+	%as% -g -mcpu=arm7tdmi -mthumb-interwork "%~dp0Patches\FE7_defs.s" -o "%~dp0Patches\FE7_defs.elf"
 
 	@rem Assebmle into a .lyn.event with definitions
-	%LYN% "%~dp0Patches/%~n1.elf" "%~dp0Patches/FE7_defs.elf" > "%~dp0Patches/FE7_%~n1.lyn.event"
+	%LYN% "%~dp0Patches\%~n1.elf" "%~dp0Patches\FE7_defs.elf" > "%~dp0Patches\FE7_%~n1.lyn.event"
 
 	@cd %~dp0/Patches
 	echo y | del "FE7_defs.elf"
@@ -36,13 +36,13 @@ if exist "FE7_defs.s" (
 ) 
 
 @cd %~dp0
-if exist "FE8_defs.s" (
+if exist "%~dp0Patches\FE8_defs.s" (
 	
 	@rem Assemble definitions into a .elf if exists	
-	%as% -g -mcpu=arm7tdmi -mthumb-interwork "FE8_defs.s" -o "%~dp0Patches/FE8_defs.elf"
+	%as% -g -mcpu=arm7tdmi -mthumb-interwork "%~dp0Patches\FE8_defs.s" -o "%~dp0Patches\FE8_defs.elf"
 
 	@rem Assebmle into a .lyn.event with definitions
-	%LYN% "%~dp0Patches/%~n1.elf" "%~dp0Patches/FE8_defs.elf" > "%~dp0Patches/FE8_%~n1.lyn.event"
+	%LYN% "%~dp0Patches\%~n1.elf" "%~dp0Patches\FE8_defs.elf" > "%~dp0Patches\FE8_%~n1.lyn.event"
 
 	@cd %~dp0/Patches
 	echo y | del "FE8_defs.elf"
@@ -50,6 +50,6 @@ if exist "FE8_defs.s" (
 ) 
 
 
-@rem echo y | del "%~n1.elf"
+echo y | del "%~n1.elf"
 
 pause
