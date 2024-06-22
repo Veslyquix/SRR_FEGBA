e0ee7cc54d6dbb452187048cc0ee7491d24989c4
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-06-22 12:43:34 -0400

    stuff

--- Patches/FE7/Gfx/Anims/_BatchAnimationAssembler.cmd
+++ Patches/FE7/Gfx/Anims/_BatchAnimationAssembler.cmd
@@ -4,20 +4,27 @@
 
 @cd %~dp0/png
 
-
-@for /R %%m in (.) do ( 
+@for /d %%i in (.\*) do ( 
+cd "%%i"  
+@for /R %%m in (.) do (
 	cd "%%m"
-	cd
-	@dir *.bin /b > bin.txt
-	@for /f "tokens=*" %%n in (bin.txt) do (
-		@echo %%~nn.event 
-		if not exist "%%~nn Installer.event" ( "%AnimationAssembler%" "%%n"  )
-	)
-	@dir *.event /b > event.txt
-	@for /f "tokens=*" %%n in (event.txt) do ( 
-		if not exist "%~dp0%%~nn Installer.event" ( echo n | @copy /-y "%%n" "%~dp0Event" > nul  )
-	)
-)  
+	@rem cd
+	if exist *.bin ( 
+		@dir *.bin /b > bin.txt
+		@for /f "tokens=*" %%n in (bin.txt) do ( 
+			@rem if exist "%%~nn Installer.event" ( @echo "%%n already processed" )
+			if not exist "%%~nn Installer.event" ( "%AnimationAssembler%" "%%n" )
+		)
+		@dir *.event /b > event.txt
+		@for /f "tokens=*" %%n in (event.txt) do ( 
+			if not exist "%~dp0Event/%%i_%%~nn.event" ( 
+			@echo "%%~nn.event"
+			@copy /-y "%%~nn.event" "%~dp0Event/%%i_%%~nn.event"  > nul 
+			)
+		)
+	)  
+) 
+)
 
 
 @rem @del bin.txt
@@ -29,6 +36,6 @@
 
 @rem @del "%~dp0bin\*.event"
 
-echo Done!
+@echo Done!
 
 pause
