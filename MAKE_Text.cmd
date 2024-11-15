324b800788e6393e2348724675d90eb7fce527f2
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-11-15 15:30:33 -0500

    fe6 test

--- MAKE_Text.cmd
+++ MAKE_Text.cmd
@@ -1,11 +1,15 @@
  @echo off
-set "textprocess=%~dp0Tools\TextProcess\text-process-classic-fe6.py"
+set "textprocess=%~dp0Tools\TextProcess\text-process-classic.py"
+set "textprocessfe6=%~dp0Tools\TextProcess\text-process-classic-fe6.py"
 set "parsefile=%~dp0EventAssembler\Tools\ParseFileUTF8.exe"
 set "source_rom=%~dp0FE8.gba"
 set "base_dir=%~dp0"
 
   cd "%base_dir%Text"
   echo: | ("%textprocess%" text_buildfile.txt --parser-exe "%parsefile%" --installer "InstallTextData.event" --definitions "TextDefinitions.event")
+
+  cd "%base_dir%TextFE6"
+  echo: | ("%textprocessfe6%" text_buildfile.txt --parser-exe "%parsefile%" --installer "InstallTextData.event" --definitions "TextDefinitions.event")
 
 
 echo:
