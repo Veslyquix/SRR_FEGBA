214544c40f8cd8d4c0dd03daeebe2bcc3e283f08
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-11-15 11:23:15 -0500

    wip

--- MAKE_Text.cmd
+++ MAKE_Text.cmd
@@ -1,10 +1,12 @@
  @echo off
-set "textprocess=%~dp0Tools\TextProcess\text-process-classic.exe"
+set "textprocess=%~dp0Tools\TextProcess\text-process-classic-fe6.py"
+set "parsefile=%~dp0EventAssembler\Tools\ParseFileUTF8.exe"
 set "source_rom=%~dp0FE8.gba"
 set "base_dir=%~dp0"
 
   cd "%base_dir%Text"
   echo: | ("%textprocess%" text_buildfile.txt --parser-exe "%parsefile%" --installer "InstallTextData.event" --definitions "TextDefinitions.event")
+
 
 echo:
 echo Done!
