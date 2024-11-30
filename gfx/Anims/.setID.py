44648dc08acd7fe9f7c3b3ebb03ec47a0026c4e3
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-11-30 13:57:41 -0500

    few more classes

--- gfx/Anims/.setID.py
+++ gfx/Anims/.setID.py
@@ -22,10 +22,11 @@ primary_keywords = ["Egyptian", "Cantor_by", "Cantor_Jedah", "Arcanist_by_Nuramo
     "Lancer_by_SALVAGED", "Militia", "Sentinel", "Tethys_Repalette", "FE8_Tethys", 
     "T1_Lancer", "Gladiator", "Hunter", "Chicken", "SupplierAnna", "Merlinus_Transport", 
     "Sandworm", "Cursed_Sword", "Tomes", "Mimic_Chest", "Mosquito", 
-    "Phantom_by_TBA", "Slime", "Warbird", "Adventurer"]
+    "Phantom_by_TBA", "Slime", "Warbird", "Adventurer", "Grand_Mahout", "Brawler",
+    "Enchanter", "Black_Dragon", "Djinn", "Living_Armor", "Fellbeast", "Brawler2"]
 
 # List of weapon types to append to the primary keyword
-weapon_keywords = ["Unarmed", "Knife", "Handaxe", "Magic", "Sword",  "Axe", "Bow", "Lance",  "Ranged", "Staff", "Refresh", "Monster"]
+weapon_keywords = ["Unarmed", "Knife", "Handaxe", "Magic", "Sword",  "Axe", "Bow", "Lance",  "Ranged", "Staff", "Refresh", "Monster", "Melee", "Supply"]
 
 anim_offset = 0  # Offset for StartingAnimID
 
