6d017cb625988a2708e76203c78cceb5d8861eb2
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-04-16 10:50:31 -0400

    remove portraits2dmp.exe test

--- gfx/Portraits/BuildExeFile.cmd
+++
@@ -1,9 +0,0 @@
-@echo off 
-
-pyinstaller --onefile --additional-hooks-dir=pyinstaller_hooks_contrib/hooks/stdhooks portraits2dmp_numba.spec
-
-
-@rem pyinstaller --onefile --hidden-import=numba --hidden-import=numba.core.decorators --hidden-import=numba.core.typing.typeddict --hidden-import=numba.cpython.unsafe.nrt --hidden-import=numba.cpython portraits2dmp.py
-
-pause 
-
