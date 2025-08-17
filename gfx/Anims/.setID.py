26d21670302aab2a32d68ba768f1278a1ef0f68f
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-08-17 13:55:01 -0400

    build success fe8

--- gfx/Anims/.setID.py
+++ gfx/Anims/.setID.py
@@ -23,7 +23,7 @@ primary_keywords = ["Egyptian", "Cantor_by", "Cantor_Jedah", "Arcanist_by_Nuramo
     "T1_Lancer", "Gladiator", "Hunter", "Chicken", "SupplierAnna", "Merlinus_Transport", 
     "Sandworm", "Cursed_Sword", "Tomes", "Mimic_Chest", "Mosquito", 
     "Phantom_by_TBA", "Slime", "Warbird", "Adventurer", "Grand_Mahout", "Brawler",
-    "Enchanter", "Black_Dragon", "Djinn", "Living_Armor", "Fellbeast", "Brawler2", "LynLord"]
+    "Enchanter", "Black_Dragon", "Djinn", "Living_Armor", "Fellbeast", "Brawler2", "LynLord", "PriestessT1"]
 
 # List of weapon types to append to the primary keyword
 weapon_keywords = ["Unarmed", "Knife", "Handaxe", "Magic", "Sword",  "Axe", "Bow", "Lance",  "Ranged", "Staff", "Refresh", "Monster", "Melee", "Supply"]
